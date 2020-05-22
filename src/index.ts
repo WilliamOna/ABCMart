3c1ba09e85a444b12d7cae9206a692508d715a27
William Ona <william.ona@revature.com>
2020-05-22 12:08:44 -0500

    testing cd

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ app.use('/paper',paperRouter);
 app.use('/auth',loginRouter)
 
 app.get('/',(req,res)=>{
-    res.send("New Version!!!")
+    res.send("New Version!!! :D")
 })
 // start the server
 app.listen(PORT,()=>{
